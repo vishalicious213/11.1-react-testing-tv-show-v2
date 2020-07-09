411c8920b65ea8d0c84626b6af140adbc277b4ca
Vish Singh <vishalicious213@gmail.com>
2020-07-09 15:03:58 -0700

    added .catch to axios call in fetchShow

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 const fetchShow = () => {
     return axios
-      .get(
-        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-      )
+        .get(
+            "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+        )
     //   .then(res => {
     //     setShow(res.data);
     //     setSeasons(formatSeasons(res.data._embedded.episodes));
     //   });
+        .catch(error => console.log('Axios error: ', error));
   };
 
   export default fetchShow;
