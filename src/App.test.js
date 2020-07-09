ca143d511de08c62e60d4b1b48d3481dfcb7e995
Vish Singh <vishalicious213@gmail.com>
2020-07-09 15:57:43 -0700

    got testing to work! it didn't work at all last time around.

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+// TEST SUITE TO MAKE SURE TESTING WORKS
+// describe('true is truthy and false is falsy', () => {
+//     test('true is truthy', () => {
+//       expect(true).toBe(true);
+//     });
+   
+//     test('false is falsy', () => {
+//       expect(false).toBe(false);
+//     });
+//   });
