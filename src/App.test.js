bfc154900d6779a271d7cf26e0125eaf731212c5
Vish Singh <vishalicious213@gmail.com>
2020-07-10 16:20:43 -0700

    test: Renders App asynchronously and axios call gets data (show Stranger Things text)

--- src/App.test.js
+++ src/App.test.js
@@ -15,15 +15,24 @@ import App from './App';
 
 test('App renders w/o crashing', () => {
     render(<App />);
-    screen.debug();
+    // screen.debug(); // shows DOM elements in tree in console
 });
 
 test('Render: Fetching data...', () => {
     render(<App />);
     expect(screen.getByText('Fetching data...')).toBeInTheDocument();
-});
+}); // Use getBy to find elements that should be in DOM
 
 test('Stranger Things title should not be in DOM', () => {
     render(<App />);
     expect(screen.queryByText(/Stranger Things/)).toBeNull();
+}); // use queryBy to assert that elements are NOT in DOM
+
+test('Renders App and axios call gets data', async () => {
+    render(<App />);
+    // expect(screen.queryByText(/Stranger Things/)).toBeNull();
+    screen.debug();
+    const findH1 = await screen.findAllByText(/Stranger Things/);
+    // console.log(findH1);
+    expect(findH1[0]).toBeInTheDocument();
 });
